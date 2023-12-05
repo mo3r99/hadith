8357341fe4dddfcdbfd3433036b86fc6eac58b62
moerefine <moe@refine.agency>
2023-12-05 22:50:35 +0000

    navbar changes and refactor

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,28 +1,70 @@
-import Styles from './Navbar.module.css'
-
-import Logo from '/src/assets/book.png'
-
-import homeLogo from '/src/assets/home.png'
-import collectionsLogo from '/src/assets/collections.png'
-import currentLogo from '/src/assets/current.png'
-import settingsLogo from '/src/assets/settings.png'
-import favouritesLogo from '/src/assets/favourites.png'
+import Styles from "./Navbar.module.css";
+import Logo from "/src/assets/book.png";
+import homeLogo from "/src/assets/home.png";
+import collectionsLogo from "/src/assets/collections.png";
+import currentLogo from "/src/assets/current.png";
+import settingsLogo from "/src/assets/settings.png";
+import favouritesLogo from "/src/assets/favourites.png";
+import "./Navbar.scss";
 
 function Navbar() {
-  return (
-    <nav>
-        <ul>
-            <li><img src={homeLogo} /><span>Home</span></li>
-            <li><img src={collectionsLogo} /><span>Collections</span></li>
-            <li className={Styles.bold}><img src={currentLogo} /><span>Current</span></li>
-            <li className={Styles.right}><img src={favouritesLogo} /><span>Favourites</span></li>
-            <li className={Styles.right}><img src={settingsLogo} /><span>Settings</span></li>
-        </ul>
-        <div className={Styles.logo}>
-            <img src={Logo} />
+    return (
+        <div className="navBar">
+            <div className="container">
+                <div className="left">
+                    <div className="item">
+                        <img src={homeLogo} />
+                        <span>Home</span>
+                    </div>
+
+                    <div className="item">
+                        <img src={collectionsLogo} />
+                        <span>Collections</span>
+                    </div>
+
+                    <div className="item">
+                        <img src={currentLogo} />
+                        <span>Current</span>
+                    </div>
+                </div>
+                <div className="middle">
+                    <img src={Logo} />
+                </div>
+                <div className="right">
+                    <div className="item">
+                        <img src={favouritesLogo} />
+                        <span>Favourites</span>
+                    </div>
+
+                    <div className="item">
+                        <img src={settingsLogo} />
+                        <span>Settings</span>
+                    </div>
+                </div>
+            </div>
         </div>
-    </nav>
-  )
+
+        // <nav className="navBar">
+        //     <ul>
+        //         <li>
+        //             <img src={homeLogo} />
+        //             <span>Home</span>
+        //         </li>
+        //         <li>
+        //             <img src={collectionsLogo} />
+        //             <span>Collections</span>
+        //         </li>
+        //         <li className={Styles.bold}>
+        //             <img src={currentLogo} />
+        //             <span>Current</span>
+        //         </li>
+
+        //     </ul>
+        //     <div className={Styles.logo}>
+        //
+        //     </div>
+        // </nav>
+    );
 }
 
-export default Navbar
+export default Navbar;
--- src/pages/singleHadithPage/SingleHadithPage.jsx
+++ src/pages/singleHadithPage/SingleHadithPage.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import "./SingleHadith.scss";
+import "./SingleHadithPage.scss";
 import HadithComponent from "../../components/hadithComponent/HadithComponent";
 import CommentaryComponent from "../../components/commentaryComponent/CommentaryComponent";
 
 const SingleHadith = () => {
-    console.log("test");
     return (
         <div className="singleHadith">
             <HadithComponent />

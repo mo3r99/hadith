398916e4a5defd28c2ba5d6470d7e4f70b564a54
mo3r99 <mo3r99@gmail.com>
2023-12-06 19:47:30 +0000

    added collections

--- src/pages/collections/Collections.jsx
+++ src/pages/collections/Collections.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "../../components/navbar/Navbar";
+import { Link } from "react-router-dom";
 
 import Styles from "./Collections.module.scss";
 import Card from "../../components/ui/card/Card";
@@ -11,9 +11,41 @@ function Collections() {
         <h3>Collections</h3>
       </header>
       <section className={Styles.collections}>
-        <Card>
-          <h2>Sahih Bukhari</h2>
-        </Card>
+        <Link to="/hadith/sahih-bukhari/1">
+          <Card>
+            <h2>Sahih Al-Bukhari</h2>
+          </Card>
+        </Link>
+
+        <Link to="/hadith/sahih-muslim/1">
+          <Card>
+            <h2>Sahih Muslim</h2>
+          </Card>
+        </Link>
+
+        <Link to="/hadith/al-tirmidhi/1">
+          <Card>
+            <h2>Jami' Tirmidhi</h2>
+          </Card>
+        </Link>
+
+        <Link to="/hadith/abu-dawood/1">
+          <Card>
+            <h2>Sunan Abi Dawood</h2>
+          </Card>
+        </Link>
+
+        <Link to="/hadith/sunan-nasai/1">
+          <Card>
+            <h2>Sunan Nasa'i</h2>
+          </Card>
+        </Link>
+
+        <Link to="/hadith/ibn-e-majah/1">
+          <Card>
+            <h2>Sunan ibn Majah</h2>
+          </Card>
+        </Link>
       </section>
     </>
   );

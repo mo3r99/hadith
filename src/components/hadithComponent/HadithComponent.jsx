5e6cc358702e1138e63871bfab9ee6eb95e0956c
moerefine <moe@refine.agency>
2023-12-06 01:18:55 +0000

    refactored code

--- src/components/hadithComponent/HadithComponent.jsx
+++ src/components/hadithComponent/HadithComponent.jsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import { useQuery } from "react-query";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import "./HadithComponent.scss";
 
 const HadithComponent = () => {
     const params = useParams();
@@ -33,39 +34,43 @@ const HadithComponent = () => {
         isError,
     } = useQuery("hadiths", getHadith);
 
-    if (isLoading) return <div>Loading...</div>;
-    if (isError) return <div>Error loading data</div>;
+    if (isLoading) return <div className="preload">فَصَبْرٌ جَمِيلٌ</div>;
+    if (isError) return <div className="preload">Error loading data</div>;
 
     console.log(hadithData?.hadiths.data);
 
     return (
         <div className="hadithComponent">
-            <header>
-                <h2>
+            <div className="heading">
+                <div className="arrow">
                     <Link to="/" className="link">
                         <span className="material-symbols-outlined back_home">
                             arrow_back_ios_new
                         </span>
                     </Link>
-
-                    {hadithData?.hadiths.data[0].book.bookName}
-                </h2>
-            </header>
+                </div>
+                <div className="book">
+                    <h2>{hadithData?.hadiths.data[0].book.bookName}</h2>
+                </div>
+            </div>
             <section className="hadith">
-                <div>
+                <div className="chapter">
                     <span>
+                        Chapter:{" "}
                         {hadithData?.hadiths.data[0].chapter.chapterEnglish}
                     </span>
-                    <span> • </span>
-                    <span>{}</span>
                 </div>
-                <div>
-                    <p className="arabic">
-                        {hadithData?.hadiths.data[0].hadithArabic}
-                    </p>
-                </div>
-                <div>
-                    <p>{hadithData?.hadiths.data[0].hadithEnglish}</p>
+
+                <div className="textContainer">
+                    <div className="hadithNumber">
+                        <h2>{hadithData?.hadiths.data[0].hadithNumber}</h2>
+                    </div>
+                    <div className="english">
+                        <p className="arabic">
+                            {hadithData?.hadiths.data[0].hadithArabic}
+                        </p>
+                        <p>{hadithData?.hadiths.data[0].hadithEnglish}</p>
+                    </div>
                 </div>
             </section>
         </div>
